cf522a6e8b21c8eacaec3d950136ad2c1066368d
Riya Singh <singhriya3408@gmail.com>
2026-05-17 15:44:52 +0530

    Fixing EOF

--- products/views.py
+++ products/views.py
@@ -9,3 +9,5 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'products/detail.html', {'product': product})
+
+    
