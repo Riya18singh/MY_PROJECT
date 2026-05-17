1797ed2035164dad82ee46bb0e341b23ae9143bc
Riya Singh <singhriya3408@gmail.com>
2026-05-17 09:52:56 +0530

    view is updated

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from.models import products
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.filter(is_active=True)
+    return render(request, 'products/list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'products/detail.html', {'product': product})
