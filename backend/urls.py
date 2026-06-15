3f8b9b4e336a9a888dda1fca7ecf3fefa994f1aa
Riya Singh <singhriya3408@gmail.com>
2026-06-15 16:10:03 +0530

    successfully implemented the order history features

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from cart import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
+    path('my-orders/', views.order_history, name='order_history'),
 ]
