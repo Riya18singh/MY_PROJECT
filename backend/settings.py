37e165a6b8d79bed6c724909018cf9c3fbbe0701
Riya Singh <singhriya3408@gmail.com>
2026-06-19 13:15:01 +0530

    Resolve conflict using env variables

--- backend/settings.py
+++ backend/settings.py
@@ -137,7 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Razorpay API Credentials Configuration
 RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
 RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
 EXCHANGE_RATE_API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
